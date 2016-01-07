6df29a7c3d3565fafdf3563dd659b2716fc7fe00
jeffssss <467318976@qq.com>
2016-01-07 14:51:42 +0800

    init project

---
+++ tumblr-p/tumblr-p/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tumblr-p
+//
+//  Created by JiFeng on 16/1/7.
+//  Copyright © 2016年 jeffsss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tumblr-p/tumblr-p/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tumblr-p
+//
+//  Created by JiFeng on 16/1/7.
+//  Copyright © 2016年 jeffsss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
